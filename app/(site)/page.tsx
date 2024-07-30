854933cf174aab0d19037390c363aa9de180e12e
ygvbhy <ygvbhy@naver.com>
2024-07-30 22:18:15 +0900

    group folder

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+// 라우트에 영향을 주지 않는 폴더 (site)
+// page.tsx 를 여기에 만들어서 Home Page 를 이 파일로 지정 할 수 있음
+import React from "react";
+
+const page = () => {
+  return <div>HomePage</div>;
+};
+
+export default page;
