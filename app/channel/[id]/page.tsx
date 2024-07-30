881b753bf7af326f10642ee307e43415fb3b2c36
ygvbhy <ygvbhy@naver.com>
2024-07-30 22:14:48 +0900

    page, params, searchParams

---
+++ app/channel/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const channel = ({ params }) => {
+  return <div>channel/[{params.id}]</div>;
+};
+
+export default channel;
---
+++ app/playlist/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const playlist = ({ searchParams }) => {
+  return <div>playlist / [{searchParams.list}]</div>;
+};
+
+export default playlist;
