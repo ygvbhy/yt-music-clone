762194f5372a1bf554918fc6da84f5fb09090f15
ygvbhy <ygvbhy@naver.com>
2024-11-15 23:41:42 +0900

    7.7 PlayListCard - 3

--- components/PlayListCard.jsx
+++ components/PlayListCard.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import Image from "next/image";
 import { getRandomElementFromArray } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import { MdMoreVert } from "react-icons/md";
+import { FiPlay } from "react-icons/fi";
+import IconButton from "./elements/IconButton";
 
 const PlayListCard = ({ playlist }) => {
   const { push } = useRouter();
@@ -14,8 +17,10 @@ const PlayListCard = ({ playlist }) => {
     push(`/playlist?list=${id}`);
   };
 
+  const onClickPlay = () => {};
+
   return (
-    <article className="h-[240px] cursor-pointer">
+    <article className="h-[240px] cursor-pointer group">
       <section className="relative h-[136px]" onClick={onClickCard}>
         <Image
           src={imgSrc}
@@ -23,6 +28,17 @@ const PlayListCard = ({ playlist }) => {
           alt="thumbnail"
           className="object-cover"
         />
+        <div className="relative hidden group-hover:block bg-gradient-to-b from-[rgba(0,0,0,0.7)] top-0 w-full h-[136px]">
+          <div className=" absolute top-2 right-4">
+            <IconButton icon={<MdMoreVert size={20} />} />
+          </div>
+          <div
+            onClick={onClickPlay}
+            className=" absolute bottom-4 right-4 flex items-center justify-center transform-gpu transition-transform hover:scale-110 bg-[rgba(0,0,0,0.7)] w-[45px] h-[45px] rounded-full hover:bg-[rgba(0,0,0,0.9)] pl-[1.5px]"
+          >
+            <FiPlay size={22} color="red" />
+          </div>
+        </div>
       </section>
       <section className="mt-2">
         <div>{playlistName}</div>
