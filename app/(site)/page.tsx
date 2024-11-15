f7c3888a65117fed93208be3b7d63f84765bf393
ygvbhy <ygvbhy@naver.com>
2024-11-15 23:03:22 +0900

    7.3 PlayListCarousel 및 타이핑

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,13 +4,31 @@
 import { sleep } from "@/lib/utils";
 import React from "react";
 import { Category } from "./components/Category.jsx";
+import PagePadding from "@/components/PagePadding.jsx";
+import PlayListCarousel from "@/components/PlayListCarousel";
+import { dummyPlaylistArray } from "@/lib/dummyData.js";
+import UserIcon from "@/components/UserIcon.jsx";
 
 const page = async () => {
+  const dummyPlaylistArray1 = [...dummyPlaylistArray];
   return (
-    <div className="min-h-[600px]">
-      <div className="mt-9"></div>
-      <Category></Category>
-    </div>
+    <PagePadding>
+      <div className="min-h-[600px]">
+        <div className="mt-9"></div>
+        <Category></Category>
+        <div className="mt-12"></div>
+        <PlayListCarousel
+          playlistArray={[...dummyPlaylistArray1]}
+          Thumnail={
+            <div className="w-[56px] h-[56px]">
+              <UserIcon />
+            </div>
+          }
+          title="다시 듣기"
+          subTitle="SK"
+        />
+      </div>
+    </PagePadding>
   );
 };
 
