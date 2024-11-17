1e49d62133f669e29ef059a91317dc312770d771
ygvbhy <ygvbhy@naver.com>
2024-11-17 13:15:41 +0900

    8.3 song card - 1

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,11 +1,15 @@
 import PagePadding from "@/components/PagePadding";
 import React from "react";
 import Category from "./components/Category";
-import { getAllPlaylist } from "@/lib/dummyData";
+import { getAllPlaylist, getSongListTop10 } from "@/lib/dummyData";
 import PlayListCarousel from "@/components/PlayListCarousel";
+import SongListCarousel from "@/components/SongListCarousel";
 
 const page = async () => {
-  const playlistArray = await getAllPlaylist();
+  const [playlistArray, songlistTop10] = await Promise.all([
+    getAllPlaylist(),
+    getSongListTop10(),
+  ]);
 
   return (
     <PagePadding>
@@ -13,6 +17,8 @@ const page = async () => {
       <Category />
       <div className="mt-20"></div>
       <PlayListCarousel playlistArray={playlistArray} title="새 앨범 및 싱글" />
+      <div className="mt-20"></div>
+      <SongListCarousel songlistTop10={songlistTop10} title="새 앨범 및 싱글" />
     </PagePadding>
   );
 };
