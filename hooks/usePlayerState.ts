50327b91c8494710dde5a6efb83e42b878a75b4d
ygvbhy <ygvbhy@naver.com>
2024-11-17 21:27:31 +0900

    12.6 zustand - usePalyerState - 2

--- hooks/usePlayerState.ts
+++ hooks/usePlayerState.ts
@@ -1,13 +1,40 @@
+import { Song } from "@/types";
 import { create } from "zustand";
 
 interface PlayerState {
   isVisiblePlayer: boolean;
   setIsVisiblePlayer: (isVisiblePlayer: boolean) => void;
+  activeSong?: Song | null;
+  prevPlayerQueue: Song[];
+  nextPlayerQueue: Song[];
+  addSongList: (songList: Song[]) => void;
+  playNext: () => void;
+  playBack: () => void;
 }
 
 const usePlayerState = create<PlayerState>((set) => ({
   isVisiblePlayer: true,
   setIsVisiblePlayer: (isVisiblePlayer: boolean) => set({ isVisiblePlayer }),
+  activeSong: null,
+  prevPlayerQueue: [],
+  nextPlayerQueue: [],
+  addSongList: (songList: Song[]) =>
+    set((prev) => {
+      const prevSong = prev.activeSong;
+      const cloneSongList = [...songList];
+      const currentSong = cloneSongList.splice(0, 1)?.[0];
+
+      return {
+        activeSong: currentSong,
+        prevPlayerQueue: prevSong
+          ? [prevSong, ...prev.prevPlayerQueue]
+          : prev.prevPlayerQueue,
+        nextPlayerQueue: [...cloneSongList],
+        isVisiblePlayer: true,
+      };
+    }),
+  playNext: () => {},
+  playBack: () => {},
 }));
 
 export default usePlayerState;
