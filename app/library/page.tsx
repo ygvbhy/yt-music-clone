6cc41f0a228936063bb83fe1b1e57378a1fb0bb6
ygvbhy <ygvbhy@naver.com>
2024-11-17 15:08:14 +0900

    9.2 dropdown Menu

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,7 +1,13 @@
+import PagePadding from "@/components/PagePadding";
 import React from "react";
+import { Category } from "./components/Category";
 
 const library = () => {
-  return <div>library</div>;
+  return (
+    <PagePadding>
+      <Category />
+    </PagePadding>
+  );
 };
 
 export default library;
