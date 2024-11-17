58e2902af7e2b247655c1e3c72fbe33cbaff231b
ygvbhy <ygvbhy@naver.com>
2024-11-17 21:06:33 +0900

    12.2 zustand - usePlayerState - 1

---
+++ hooks/usePlayerState.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface PlayerState {
+  isVisiblePlayer: boolean;
+  setIsVisiblePlayer: (isVisiblePlayer: boolean) => void;
+}
+
+const usePlayerState = create<PlayerState>((set) => ({
+  isVisiblePlayer: false,
+  setIsVisiblePlayer: (isVisiblePlayer: boolean) => set({ isVisiblePlayer }),
+}));
+
+export default usePlayerState;
