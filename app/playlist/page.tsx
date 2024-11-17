cb997c1155721031416247b931ef17e5becdc136
ygvbhy <ygvbhy@naver.com>
2024-11-17 16:54:46 +0900

    10.6 SongList Link

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import PagePadding from "@/components/PagePadding";
 import PlaylistHead from "@/components/PlaylistHead";
+import SongCardRowExpand from "@/components/SongCardRowExpand";
 
 interface PlaylistPageProps {
   searchParams: {
@@ -24,6 +25,12 @@ const page = async (props: PlaylistPageProps) => {
       <HeaderBgChanger imageSrc={imageSrc} />
       <div className="mt-12"></div>
       <PlaylistHead playlist={playlist} />
+      <div className="mt-12"></div>
+      <section className="flex flex-col gap-2">
+        {playlist.songList.map((song, idx) => {
+          return <SongCardRowExpand song={song} key={idx} />;
+        })}
+      </section>
     </PagePadding>
   );
 };
