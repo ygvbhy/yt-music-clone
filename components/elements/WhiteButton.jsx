36c195d11b76dbe4ebac9b71ed55a05fa76d0268
ygvbhy <ygvbhy@naver.com>
2024-11-17 16:45:47 +0900

    10.4 Header 반응형

---
+++ components/elements/WhiteButton.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const WhiteButton = ({ icon, label, className, ...props }) => {
+  return (
+    <div
+      className={cn(
+        " cursor-pointer bg-white text-black hover:bg-neutral-200 rounded-2xl flex flex-row items-center min-w-[80px] max-w-[100px] h-[36px] p-4 gap-2",
+        className
+      )}
+      {...props}
+    >
+      <span>{icon}</span>
+      <span>{label}</span>
+    </div>
+  );
+};
+
+export default WhiteButton;
