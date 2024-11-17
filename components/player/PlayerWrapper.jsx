d3a0d8aa55c1ce44d3032393c56c2da65c9b8eb5
ygvbhy <ygvbhy@naver.com>
2024-11-17 21:13:39 +0900

    12.4 PlayerWrapper > Height

--- components/player/PlayerWrapper.jsx
+++ components/player/PlayerWrapper.jsx
@@ -1,6 +1,12 @@
+"use client";
 import React from "react";
+import usePlayerState from "@/hooks/usePlayerState";
 
 const PlayerWrapper = () => {
+  const { isVisiblePlayer } = usePlayerState();
+
+  if (!isVisiblePlayer) return null;
+
   return (
     <div className=" fixed bottom-0 h-[72px] w-full bg-neutral-900">
       PlayerWrapper
