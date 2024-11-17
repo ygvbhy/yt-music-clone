fe6e6b2405e68c0cb0212de858cf3054591e8542
ygvbhy <ygvbhy@naver.com>
2024-11-17 21:50:15 +0900

    12.9 Slide 노래 재생 연동

--- components/player/PlayerContent.jsx
+++ components/player/PlayerContent.jsx
@@ -1,33 +1,68 @@
+"use client";
 import React from "react";
 import { Slider as PlayerSlider } from "@/components/ui/playerSlider";
 import { useAudio } from "react-use";
+import { IoPlaySkipBackSharp, IoPlaySkipForwardSharp } from "react-icons/io5";
+import { AiOutlinePause } from "react-icons/ai";
+import usePlayerState from "@/hooks/usePlayerState";
+import { ClipLoader } from "react-spinners";
+import { RiPlayFill } from "react-icons/ri";
 
 const PlayerContent = () => {
+  const { activeSong } = usePlayerState();
   const [audio, state, controls, ref] = useAudio({
-    src: null,
+    src: activeSong?.src,
     autoPlay: true,
   });
+
+  const isLoading = activeSong?.src && state.buffered?.length === 0;
+
+  const onClickPreBtn = () => {};
+  const onClickNextBtn = () => {};
+  const onClickStartBtn = () => {
+    controls.play();
+  };
+  const onClickPauseBtn = () => {
+    controls.pause();
+  };
+
   return (
     <div className="h-full w-full relative">
       <div className=" absolute top-[-16px] w-full">
         <PlayerSlider />
       </div>
-      <div>
-        {audio}
-        {/* <pre>{JSON.stringify(state, null, 2)}</pre>
-        <button onClick={controls.pause}>Pause</button>
-        <button onClick={controls.play}>Play</button>
-        <br />
-        <button onClick={controls.mute}>Mute</button>
-        <button onClick={controls.unmute}>Un-mute</button>
-        <br />
-        <button onClick={() => controls.volume(0.1)}>Volume: 10%</button>
-        <button onClick={() => controls.volume(0.5)}>Volume: 50%</button>
-        <button onClick={() => controls.volume(1)}>Volume: 100%</button>
-        <br />
-        <button onClick={() => controls.seek(state.time - 5)}>-5 sec</button>
-        <button onClick={() => controls.seek(state.time + 5)}>+5 sec</button> */}
-      </div>
+      {audio}
+      <section className="flex flex-row justify-between items-center w-full h-full px-2 lg:px-6">
+        <div className="h-full flex flxe-row items-center gap-1">
+          <IoPlaySkipBackSharp
+            size={24}
+            className=" cursor-pointer"
+            onClick={onClickPreBtn}
+          />
+          {isLoading ? (
+            <ClipLoader color="#fff" />
+          ) : state.playing ? (
+            <AiOutlinePause
+              size={40}
+              className=" cursor-pointer"
+              onClick={onClickPauseBtn}
+            />
+          ) : (
+            <RiPlayFill
+              size={40}
+              className=" cursor-pointer"
+              onClick={onClickStartBtn}
+            />
+          )}
+          <IoPlaySkipForwardSharp
+            size={24}
+            className=" cursor-pointer"
+            onClick={onClickNextBtn}
+          />
+        </div>
+        <article></article>
+        <div></div>
+      </section>
     </div>
   );
 };
