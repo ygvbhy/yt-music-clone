785614366f8d916b2d8d32d2f9978c1d17d740ed
ygvbhy <ygvbhy@naver.com>
2024-11-17 21:10:00 +0900

    12.3 PlayerWrapper

--- components/player/PlayerWrapper.jsx
+++ components/player/PlayerWrapper.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const PlayerWrapper = () => {
-  return <div>PlayerWrapper</div>;
+  return (
+    <div className=" fixed bottom-0 h-[72px] w-full bg-neutral-900">
+      PlayerWrapper
+    </div>
+  );
 };
 
 export default PlayerWrapper;
