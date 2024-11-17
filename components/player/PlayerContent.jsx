000298e9e69295c8540621825c5281e2ca9e60dc
ygvbhy <ygvbhy@naver.com>
2024-11-17 21:35:37 +0900

    12.8 useAudio

--- components/player/PlayerContent.jsx
+++ components/player/PlayerContent.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { Slider as PlayerSlider } from "@/components/ui/playerSlider";
+import { useAudio } from "react-use";
 
 const PlayerContent = () => {
+  const [audio, state, controls, ref] = useAudio({
+    src: null,
+    autoPlay: true,
+  });
   return (
     <div className="h-full w-full relative">
       <div className=" absolute top-[-16px] w-full">
         <PlayerSlider />
+      </div>
+      <div>
+        {audio}
+        {/* <pre>{JSON.stringify(state, null, 2)}</pre>
+        <button onClick={controls.pause}>Pause</button>
+        <button onClick={controls.play}>Play</button>
+        <br />
+        <button onClick={controls.mute}>Mute</button>
+        <button onClick={controls.unmute}>Un-mute</button>
+        <br />
+        <button onClick={() => controls.volume(0.1)}>Volume: 10%</button>
+        <button onClick={() => controls.volume(0.5)}>Volume: 50%</button>
+        <button onClick={() => controls.volume(1)}>Volume: 100%</button>
+        <br />
+        <button onClick={() => controls.seek(state.time - 5)}>-5 sec</button>
+        <button onClick={() => controls.seek(state.time + 5)}>+5 sec</button> */}
       </div>
     </div>
   );
